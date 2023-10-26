d502df0d1f030f24a954b734df34c84b1cbb9a7a
Sifatul Islam <mdshepatul.islam@gmail.com>
2023-10-26 13:00:42 +0600

    service section set database

---
+++ src/Pages/BookService/BookService.jsx
@@ -0,0 +1,70 @@
+import { useContext } from "react";
+import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../../provider/AuthProvider";
+
+const BookService = () => {
+    const service = useLoaderData();
+    const { title, _id, price } = service;
+    const {user} = useContext(AuthContext)
+
+
+    const handleBookService = event => {
+        event.preventDefault();
+
+        const form = event.target;
+        const name = form.name.value;
+        const date = form.date.value;
+        const email = form?.email;
+        const order = {
+            customerName: name,
+            email,
+            date,
+            service: _id,
+            price: price
+        }
+
+        console.log(order)
+    }
+    return (  
+        <div>
+            <h1 className="text-center text-3xl">Book Service: {title}</h1>
+
+            <form onSubmit={handleBookService}>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <input type="text" defaultValue={user?.displayName} placeholder="input name" name="name" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Date</span>
+                        </label>
+                        <input type="date" name="date" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="email" defaultValue={user?.email} placeholder="email" name="email" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Due Amount</span>
+                        </label>
+                        <input type="text" defaultValue={'$'+ price} className="input input-bordered" required />
+                    </div>
+                </div>
+                <div className="form-control mt-6">
+                    <input className="btn bg-orange-600 text-white btn-block" type="submit" value="Order Confirm" />
+                </div>
+            </form>
+            <div className="card-body">
+
+            </div>
+        </div>
+    );
+};
+
+export default BookService;
