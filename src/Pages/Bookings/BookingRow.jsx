0781864114e91dc998adda5c554753f6df3c2385
Sifatul Islam <mdshepatul.islam@gmail.com>
2023-10-29 16:12:05 +0600

    update toggle added

--- src/Pages/Bookings/BookingRow.jsx
+++ src/Pages/Bookings/BookingRow.jsx
@@ -1,7 +1,7 @@
 
 
-const BookingRow = ({ booking, handleDelete }) => {
-    const { _id, date, service, price, img } = booking;
+const BookingRow = ({ booking, handleDelete, handleBookingConfirm }) => {
+    const { _id, date, service, price, img, status } = booking;
 
    
 
@@ -25,7 +25,10 @@ const BookingRow = ({ booking, handleDelete }) => {
             <td>{date}</td>
             <td>${price}</td>
             <th>
-                <button className="btn btn-ghost btn-xs">details</button>
+                {
+                    status === 'confirm' ? <span className="font-bold text-primary">Confirmed</span> 
+                    :
+                    <button onClick={() => handleBookingConfirm(_id)} className="btn btn-ghost btn-xs">Please Confirm</button>}
             </th>
         </tr>
     );
