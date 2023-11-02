6a4e0645dbc10f0de84229c858703b1a8961cfe8
Sifatul Islam <mdshepatul.islam@gmail.com>
2023-11-02 08:38:23 +0600

    created custrom hook

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth"
 import app from "../firebase/firebase.config";
+import axios from "axios";
 
 
 
@@ -31,9 +32,29 @@ const AuthProvider = ({ children }) => {
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            const userEmail = currentUser?.email || user?.email;
+            const loggedUser = { email: userEmail };
             setUser(currentUser);
             console.log('Current User', currentUser);
-            setLoading(false)
+            setLoading(false);
+            // if user exists then issue a token
+
+            if (currentUser) {
+                 axios.post('http://localhost:5000/jwt', loggedUser, {
+                    withCredentials: true
+                })
+                    .then(res => {
+                        console.log('token response', res.data)
+                    })
+            }
+            else{
+                axios.post('http://localhost:5000/logout', loggedUser, {
+                    withCredentials: true
+                })
+                .then(res =>{
+                    console.log(res.data)
+                })
+            }
         });
         return () => {
             return unsubscribe();
